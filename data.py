d9b8d9b04a92f9ecbb14f6816a32cc2be225cd3c
Ann131311 <143504117+Ann131311@users.noreply.github.com>
2023-09-29 18:13:05 +0300

    Add files via upload

---
+++ data.py
@@ -0,0 +1,17 @@
+headers = {
+    "Content-Type": "application/json"
+}
+
+creation_order = {
+    "firstName": "Нюся",
+    "lastName": "Волкова",
+    "address": "2",
+    "metroStation": 13,
+    "phone": "+79257945313",
+    "rentTime": 8,
+    "deliveryDate": "2023-10-16",
+    "comment": "Приветик!",
+    "color": [
+        "BLACK"
+    ]
+}
